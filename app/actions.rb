16e336e1d16a6efe221b3bda5f942a2b138156b8
LeahChurchley <vagrant@lighthouse-labs>
2016-04-17 20:33:34 +0000

    finished 6 and 7

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,18 @@ get '/messages' do
   @messages = Message.all
   erb :'messages/index'
 end
+
+get '/messages/new' do
+  @message = Message.new(
+    title: params[:content],
+    content: params[:content],
+    author: params[:author]
+  )
+  @message.save
+  redirect '/messages'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
